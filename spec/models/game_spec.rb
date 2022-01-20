e6549c3a779af9eed4d1ad3c2ae46e95a63c2342
arkhai <kiriam@bk.ru>
2022-01-20 20:47:57 +0300

    Test for game status

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -71,18 +71,47 @@ RSpec.describe Game, type: :model do
       expect(game_w_questions.status).to eq(:in_progress)
       expect(game_w_questions.finished?).to be_falsey
     end
+
+    it 'take_money! finishes the game' do
+      question = game_w_questions.current_game_question
+      game_w_questions.answer_current_question!(question.correct_answer_key)
+
+      game_w_questions.take_money!
+      prize = game_w_questions.prize
+
+      expect(game_w_questions.status).to eq(:money)
+      expect(game_w_questions.finished?).to be_truthy
+      expect(prize).to be > 0
+      expect(user.balance).to eq prize
+    end
   end
 
-  it 'take_money! finishes the game' do
-    question = game_w_questions.current_game_question
-    game_w_questions.answer_current_question!(question.correct_answer_key)
+  context '.status' do
+    before(:each) do
+      game_w_questions.finished_at = Time.now
+      expect(game_w_questions.finished?).to be_truthy
+    end
 
-    game_w_questions.take_money!
-    prize = game_w_questions.prize
+    it ':won' do
+      game_w_questions.current_level = Question::QUESTION_LEVELS.max + 1
+      expect(game_w_questions.status).to eq(:won)
+    end
 
-    expect(game_w_questions.status).to eq(:money)
-    expect(game_w_questions.finished?).to be_truthy
-    expect(prize).to be > 0
-    expect(user.balance).to eq prize
+    it ':timeout' do
+      game_w_questions.created_at = 1.hour.ago
+      game_w_questions.is_failed = true
+      expect(game_w_questions.status).to eq(:timeout)
+    end
+
+    it ':fail' do
+      game_w_questions.is_failed = true
+      expect(game_w_questions.status).to eq(:fail)
+    end
+
+    it ':money' do
+      expect(game_w_questions.status).to eq(:money)
+    end
   end
+
+
 end
