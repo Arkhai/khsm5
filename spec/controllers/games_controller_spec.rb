e19c23b1c299da1149242c72fddede700c861ba0
arkhai <kiriam@bk.ru>
2022-01-26 17:55:34 +0300

    Test for GamesController #help#fifty_fifty

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -210,6 +210,22 @@ RSpec.describe GamesController, type: :controller do
         expect(game.current_game_question.help_hash[:audience_help].keys).to contain_exactly('a', 'b', 'c', 'd')
         expect(response).to redirect_to(game_path(game))
       end
+
+      it 'uses fifty fifty' do
+        sign_in user
+        expect(game_w_questions.current_game_question.help_hash[:fifty_fifty]).not_to be
+        expect(game_w_questions.fifty_fifty_used).to be_falsey
+
+        put :help, params: {id: game_w_questions.id, help_type: :fifty_fifty}
+        game = assigns(:game)
+
+        expect(game.finished?).to be_falsey
+        expect(game.fifty_fifty_used).to be_truthy
+        expect(game.current_game_question.help_hash[:fifty_fifty]).to be
+        expect(game.current_game_question.help_hash[:fifty_fifty]).to include('b')
+        expect(game.current_game_question.help_hash[:fifty_fifty].size).to eq 2
+        expect(response).to redirect_to(game_path(game))
+      end
     end
   end
 end
